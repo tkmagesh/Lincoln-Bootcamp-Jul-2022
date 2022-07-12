4b54e632021e0b33016713bdad95b945e375ae3e
Magesh Kuppan <tkmagesh77@gmail.com>
2022-07-13 02:18:54 +0530

    memoization

---
+++ Day-02/memoize.js
@@ -0,0 +1,30 @@
+
+function memoize(pureFn){
+    var cache = {};
+
+    return function(no){
+        if (no in cache)
+            return cache[no];
+
+        console.log('processing ', no)
+        cache[no] = pureFn(no)
+        return cache[no]
+    }
+}
+
+function checkPrime(no){
+    for ( var i = 2; i <= (no/2); i++ ){
+        if (no % i === 0)
+            return false;
+    }
+    return true
+}
+
+var isPrime = memoize(checkPrime)
+
+
+function checkOddOrEven(no){
+    return no % 2 === 0 ? 'even' : 'odd';
+}
+var isOddOrEven = memoize(checkOddOrEven);
+  
