f9b293d934efc9c30b4b8827ca6f20fe6dc8de30
Magesh Kuppan <tkmagesh77@gmail.com>
2022-07-12 21:40:48 +0530

    add - implementation - complete

--- Day-01/language/calculator.js
+++ Day-01/language/calculator.js
@@ -1,6 +1,11 @@
 
-function add(x,y){
-    return x + y;
+function add(){
+    function parseArg(n){
+        if (Array.isArray(n)) return add.apply(this, n) // array 'n' is converted as an argument list to the add function
+        if (typeof n === 'function') return parseArg(n());
+        return isNaN(n) ? 0 : parseInt(n);
+    }
+    return arguments.length <= 1 ? parseArg(arguments[0]) : parseArg(arguments[0]) + add([].slice.call(arguments,1))
 }
 
 module.exports = add;
--- Day-01/language/calculator.test.js
+++ Day-01/language/calculator.test.js
@@ -15,3 +15,149 @@ test('Should add 2 numbers', function(){
     //Assert
     expect(actualResult).toBe(expectedResult);
 })
+
+test('Should add numbers in string format', function(){
+    //Arrange
+    var expectedResult = 30;
+
+    //Act
+    var actualResult = add("10",20);
+
+    //Assert
+    expect(actualResult).toBe(expectedResult);
+})
+
+
+
+test('Should ignore strings that cannot be converted to numbers', function(){
+    //Arrange
+    var expectedResult = 20;
+
+    //Act
+    var actualResult = add("abc",20);
+
+    //Assert
+    expect(actualResult).toBe(expectedResult);
+})
+
+test('Should add functions return numbers', function(){
+    //Arrange
+    var expectedResult = 30;
+
+    //Act
+    var actualResult = add(function(){ return 10;}, function(){ return 20;}) 
+
+    //Assert
+    expect(actualResult).toBe(expectedResult);
+})
+
+test('Should add functions returning numbers in string format', function(){
+    //Arrange
+    var expectedResult = 30;
+
+    //Act
+    var actualResult = add(function(){ return 10;}, function(){ return "20";}) 
+
+    //Assert
+    expect(actualResult).toBe(expectedResult);
+})
+
+test('Should return 0 when invoked with no arguments', function(){
+    //Arrange
+    var expectedResult = 0;
+
+    //Act
+    var actualResult = add() 
+
+    //Assert
+    expect(actualResult).toBe(expectedResult);
+})
+
+test('Should varying number of arguments', function(){
+    //Arrange
+    var expectedResult = 150;
+
+    //Act
+    var actualResult = add(10,20,30,40,50);
+
+    //Assert
+    expect(actualResult).toBe(expectedResult);
+})
+
+test('Should add array of numbers', function(){
+    //Arrange
+    var expectedResult = 100;
+
+    //Act
+    var actualResult = add([10,20],[30,40]);
+
+    //Assert
+    expect(actualResult).toBe(expectedResult);
+})
+
+test('Should add array of numbers in string format', function(){
+    //Arrange
+    var expectedResult = 100;
+
+    //Act
+    var actualResult = add([10,20],[30,"40"]);
+
+    //Assert
+    expect(actualResult).toBe(expectedResult);
+})
+
+test('Should add nested array of numbers and numbers in string format', function(){
+    //Arrange
+    var expectedResult = 150;
+
+    //Act
+    var actualResult = add([10,20],[[30,"40"],50]);
+
+    //Assert
+    expect(actualResult).toBe(expectedResult);
+})
+
+test('Should add arrays with strings that cannot be converted to numbers', function(){
+    //Arrange
+    var expectedResult = 60;
+
+    //Act
+    var actualResult = add([10,20], [30,"abc"]);
+
+    //Assert
+    expect(actualResult).toBe(expectedResult);
+})
+
+test('Should add functions return array of numbers and strings', function(){
+    //Arrange
+    var expectedResult = 60;
+
+    //Act
+    var actualResult =add(function(){ return 10;}, function(){ return ["20", 30];});
+
+    //Assert
+    expect(actualResult).toBe(expectedResult);
+})
+
+test('Should add array of functions return array of numbers and strings', function(){
+    //Arrange
+    var expectedResult = 60;
+
+    //Act
+    var actualResult = add([function(){ return 10;}, function(){ return ["20", 30];}])
+
+    //Assert
+    expect(actualResult).toBe(expectedResult);
+})
+
+test('Should add functions returning array of functions return array of numbers and strings', function(){
+    //Arrange
+    var expectedResult = 60;
+
+    //Act
+    var actualResult = add(function(){ return [function(){ return 10;}, function(){ return ["20", 30];}]; })
+
+    //Assert
+    expect(actualResult).toBe(expectedResult);
+})
+
