8d2563716438f3393a4f4a30639a7d72a2b60767
Magesh Kuppan <tkmagesh77@gmail.com>
2022-07-22 02:16:02 +0530

    Directives - ngClass & ngIf

--- Day-09/first-app/src/app/directives/directives.component.ts
+++ Day-09/first-app/src/app/directives/directives.component.ts
@@ -8,5 +8,7 @@ import { Component } from "@angular/core";
 })
 export class DirectivesComponent{
     applyStyle : boolean = false;
+    show : boolean = false;
+    
     classNames =  ['first', 'second', 'third', 'fourth'];
 }
