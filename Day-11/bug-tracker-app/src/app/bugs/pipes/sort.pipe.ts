fd4d03f768925f41e8edf89bba8b925697a714fb
Magesh Kuppan <tkmagesh77@gmail.com>
2022-07-26 19:47:37 +0530

    typesafe 'sort' pipe

--- Day-11/bug-tracker-app/src/app/bugs/pipes/sort.pipe.ts
+++ Day-11/bug-tracker-app/src/app/bugs/pipes/sort.pipe.ts
@@ -4,17 +4,17 @@ import { Pipe, PipeTransform } from "@angular/core";
 @Pipe({
     name : 'sort'
 })
-export class SortPipe implements PipeTransform{
+export class SortPipe<T, K extends keyof T> implements PipeTransform{
 
-    private getComparer(attrName : string){
-        return function(item1 : any, item2 : any) : number {
+    private getComparer(attrName : K){
+        return function(item1 : T, item2 : T) : number {
             if (item1[attrName] < item2[attrName]) return -1;
             if (item1[attrName] > item2[attrName]) return 1;
             return 0;
         }
     }
 
-    transform(list: any[], attrName : string) : any[] {
+    transform(list: T[], attrName : K) : T[] {
         if (!list || !list.length || !attrName) return list;
         let comparer = this.getComparer(attrName);
         list.sort(comparer);
