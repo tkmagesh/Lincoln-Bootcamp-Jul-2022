bc09d0c0b092912729050941cd75df2a7a64b99c
Magesh Kuppan <tkmagesh77@gmail.com>
2022-08-02 00:58:40 +0530

    updates

--- Day-16/rxjs-demo/20-switchMap.js
+++ Day-16/rxjs-demo/20-switchMap.js
@@ -1,25 +1,26 @@
+const { Subject, interval } = require('rxjs');
+const { switchMap, filter } = require('rxjs/operators');
 
-const { Subject, interval} = require('rxjs');
-const { switchMap, filter } = require('rxjs/operators')
-
+// A timer observable that emits every 1sec
 const timer$ = interval(1000)
 
-const switch$ = new Subject()
+// The subject that will be used a switch
+const switch$ = new Subject();
 
-const subscription = switch$
-    .pipe(
-        filter(toggle => toggle),
-        switchMap(toggle => timer$)
-    )
-    .subscribe(time => console.log('ms passed:' + time))
+// Emit timer data only when the switch is on
+const subscription = switch$.pipe(
+    filter(toggle => toggle),
+    switchMap(toggle => timer$)
+).subscribe(time => console.log('ms passed: ' + time))
 
-//Simulation
-setTimeout(() => switch$.next(true), 5000)
-setTimeout(() => switch$.next(false), 7000)
-//setTimeout(() => console.log('---------'), 5500)
 
-setTimeout(() => switch$.next(true), 10000)
-setTimeout(() => switch$.next(false), 12000)
+// Simulation
+setTimeout(() => switch$.next(true), 1000);
+setTimeout(() => switch$.next(false), 4000);
+setTimeout(() => console.log('-------'), 5500);
 
-//setTimeout(() => switch$.next(true), 1000)
-setTimeout(() => subscription.unsubscribe(), 25000)
+setTimeout(() => switch$.next(true), 6000);
+
+setTimeout(() => switch$.next(false), 7000);
+
+setTimeout(() => subscription.unsubscribe(), 10000);
