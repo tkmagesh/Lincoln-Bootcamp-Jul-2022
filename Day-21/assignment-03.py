bae488e3d326ea13f8e8abff13c7a0feebd97ce5
Magesh Kuppan <tkmagesh77@gmail.com>
2022-08-09 21:29:39 +0530

    assignment-03 solution (partial)

--- Day-21/assignment-03.py
+++ Day-21/assignment-03.py
@@ -6,3 +6,19 @@ str = 'Qui labore Lorem ullamco esse adipisicing ad eu cupidatat commodo ea ulla
     Ex Output:
         5 letter words occurs the most with 15 occurrances    
 """
+
+words = str.split(' ')
+words_count_by_size = {}
+
+for word in words:
+    word_size = len(word)
+    """
+    if word_size not in words_count_by_size:
+        words_count_by_size[word_size] = 0
+    """
+    words_count_by_size.setdefault(word_size, 0)
+    words_count_by_size[word_size] += 1
+
+print(words_count_by_size)
+
+# find the size of the word with maximum occurances
