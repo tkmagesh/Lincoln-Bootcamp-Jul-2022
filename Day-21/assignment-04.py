d297f9312532ecb3ff81bb1c105db685a3ab3701
Magesh Kuppan <tkmagesh77@gmail.com>
2022-08-10 00:07:58 +0530

    assignment-04 - solution

--- Day-21/assignment-04.py
+++ Day-21/assignment-04.py
@@ -1,10 +1,16 @@
 
 
 def add(x,y):
-    print('invocation started')
     result = x + y
-    print('invocation completed')
     return result
+
+""" 
+def log_add(x,y):
+    print('invocation started')
+    result = add(x,y)
+    print('invocation completed')
+    print(result) 
+"""
 
 def subtract(x, y):
     return x - y
@@ -16,10 +22,47 @@ def divide(x, y):
     return x / y
 
 
+""" 
+def log_operation(operation, x, y):
+    print('invocation started')
+    result = operation(x, y)
+    print('invocation completed')
+    print(result) 
+"""
+
+def log(operation):
+    def logOperation(x,y):
+        print('invocation started')
+        result = operation(x, y)
+        print('invocation completed')
+        print(result)
+    return logOperation
+
+log_add = log(add)
+log_subtract = log(subtract)
+log_multiply = log(multiply)
+log_divide = log(divide)
+
+
 def process(x,y):
-    print(add(x,y))
+    #log_add(x,y)
+    """ 
+    log_operation(add, 100, 200)
+    log_operation(subtract, 100, 200)
+    log_operation(multiply, 100, 200)
+    log_operation(divide, 100, 200) 
+    """
+
+    
+    log_add(100,200)
+    log_subtract(100, 200)
+    log_multiply(100, 200)
+    log_divide(100, 200)
+    
+    """ 
     print(subtract(x,y))
     print(multiply(x,y))
-    print(divide(x,y))
+    print(divide(x,y)) 
+    """
 
 process(100,200)
