88059d951d4794dd8f449fc7ff3a10a784fa6cc8
Magesh Kuppan <tkmagesh77@gmail.com>
2022-08-10 21:02:37 +0530

    reduce - assignment - solution

--- Day-21/15-functional-apis.py
+++ Day-21/15-functional-apis.py
@@ -25,15 +25,27 @@ list(map(lambda x, y: x * y, list1, list2))
 
 #reduce
 
-import functools
-functools.reduce("""  """)
+""" import functools
+functools.reduce("""  """) """
 
 from functools import reduce
-reduce("""  """)
+
 
 tens = [10,20,30,40,50]
 reduce(lambda x,y: x + y, tens)
+reduce(lambda x, y: x + y, tens, 1000)
 
 nos = [3,1,2,5,6,4,9,8]
 #find the count of odd and even numbers using the reduce function
+def update_even_odd_count(result, no):
+    if no % 2 == 0:
+        result['even'] += 1
+    else:
+        result['odd'] += 1
+    return result
+
+
+even_odd_count = reduce(update_even_odd_count, nos, {'even': 0, 'odd': 0})
+print(even_odd_count)
+
 
