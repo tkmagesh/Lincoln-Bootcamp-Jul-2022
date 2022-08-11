ee7d62d0be48c68c0b123bcd47dd33212aa4a6aa
Magesh Kuppan <tkmagesh77@gmail.com>
2022-08-11 21:39:55 +0530

    static attributes & methods

--- Day-21/18-oop/bank.py
+++ Day-21/18-oop/bank.py
@@ -3,13 +3,15 @@ from functools import reduce
 
 class BankAccount:
 
-    def __init__(self, name, initial_balance):
+    __account_count = 0 #class member OR static member
 
+    def __init__(self, name, initial_balance):
         self.name = name  
         self.__transactions = tuple()
         #self.__balance = initial_balance  # __ makes the attribute private
         self.deposit(initial_balance)
         print('a new bank account is created')
+        self.__class__.__account_count += 1
 
     def who_am_i(self):
         print(f"I am the bank account for {self.name}")
@@ -26,8 +28,6 @@ class BankAccount:
         else:
             print('Cannot withdraw more than the balance') 
        
-
-    
     def history(self):
         print(self.__transactions) 
    
@@ -53,7 +53,7 @@ class BankAccount:
     def __eq__(self, other):
         return (self.name == other.name) and (self.balance == other.balance)
 
-    
+
     def __add__(self, other):
         return BankAccount(f"{self.name} {other.name}", self.balance + other.balance)
 
@@ -62,6 +62,12 @@ class BankAccount:
 
     def __getitem__(self, key):
         return self.__transactions[key]
+
+    @classmethod
+    def get_account_count(cls):
+        return cls.__account_count
+
+        
 
 """ class BankAccount:
 
@@ -109,3 +115,7 @@ if (__name__ == '__main__'):
 
     print(f"transaction[0] = {account[0]}")
     print(f"transaction[1:3] = {account[1:3]}")
+
+    account2 = BankAccount('Alex', 10000)
+    print(f"balance = {account.balance}")
+    print(f"# of accounts = {BankAccount.get_account_count()}")
