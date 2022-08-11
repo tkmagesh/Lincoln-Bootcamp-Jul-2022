7e40a8202df2961465a3f8779bb816d0ff086825
Magesh Kuppan <tkmagesh77@gmail.com>
2022-08-12 00:11:44 +0530

    singleton

---
+++ Day-21/18-oop/singleton.py
@@ -0,0 +1,11 @@
+
+
+class Singleton(object):
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+        return cls._instance
+
+
