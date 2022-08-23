31c9b5536bbf89c4cfb9b13d6250bdfa126f3e3a
Magesh Kuppan <tkmagesh77@gmail.com>
2022-08-23 20:37:22 +0530

    querying by relationships

--- Day-26/models.py
+++ Day-26/models.py
@@ -1,6 +1,6 @@
 
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import declarative_base, relationship, backref
 
 Base = declarative_base()
 
@@ -12,9 +12,25 @@ class Author(Base):
     first_name = Column(String)
     last_name = Column(String)
 
+    #1-to-many relationship
+    books = relationship("Book", backref=backref("author"), cascade="all, delete-orphan")
+
     def __repr__(self):
         return (
             f"Author(author_id=({self.author_id!r}), "
             f"first_name={self.first_name!r}, last_name={self.last_name!r}"
         )
 
+
+class Book(Base):
+    __tablename__ = "book"
+
+    book_id = Column(Integer, primary_key=True)
+    title = Column(String)
+    author_id = Column(Integer, ForeignKey("author.author_id"), nullable=False)
+
+    def __repr__(self):
+        return (
+            f"Book(book_id={self.book_id!r}, "
+            f"title={self.title!r}, author_id={self.author_id!r})"
+        )
--- Day-26/orm.py
+++ Day-26/orm.py
@@ -1,9 +1,39 @@
 from sqlalchemy import create_engine, select
 from sqlalchemy.orm import Session
-from models import Author
+from models import Author, Book
 
 engine = create_engine("sqlite:///./books.db", echo=True, future=True)
 session = Session(engine)
 stmt = select(Author)
 for author in session.scalars(stmt):
     print(author)
+
+#Adding an author
+david = Author(first_name="David", last_name="Walliams")
+session.add(david)
+session.commit()
+
+#Adding multiple authors
+author_1 = Author(first_name="f1", last_name="l1")
+author_2 = Author(first_name="f2", last_name="l2")
+session.add_all([author_1, author_2])
+session.commit()
+
+#Updating an author
+author_1.first_name = "Tom"
+author_1.last_name = "Kruger"
+session.commit()
+
+#Querying an author
+stmt = select(Author).filter_by(author_id=4)
+result = session.execute(stmt).scalars().all()
+
+#Deleting an author
+session.delete(author_2)
+session.commit()
+
+#Accessing related objects
+for author in session.scalars(stmt):
+    print(author.first_name)
+    print(author.books)
+    print("-" * 40)
