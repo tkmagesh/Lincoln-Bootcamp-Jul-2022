aef1e049c4de38146645fbb95c144fc7b73df0ab
Magesh Kuppan <tkmagesh77@gmail.com>
2022-08-25 21:17:22 +0530

    listing & adding bugs in the database

--- Day-29/app.py
+++ Day-29/app.py
@@ -1,5 +1,5 @@
 
-from flask import Flask
+from flask import Flask, jsonify
 from flask_restful import Api, Resource, reqparse, inputs
 from flask_sqlalchemy import SQLAlchemy
 from datetime import date
@@ -27,6 +27,15 @@ class BugModel(db.Model):
     def __repr__(self):
         return '<Bug %r>' % self.name
 
+    def to_json(self):
+        return {
+            'id' : self.id,
+            'name' : self.name,
+            'is_closed' : self.is_closed,
+            'created_at' : self.created_at
+        }
+
+
     @classmethod
     def get_all(cls):
         return cls.query.all()
@@ -40,7 +49,7 @@ _new_bug_parser.add_argument('name',
                             help='The field cannot be blank', 
                             required=True
                             )
-_new_bug_parser.add_argument('isClosed',
+_new_bug_parser.add_argument('is_closed',
                             type=inputs.boolean,
                             default=False
                             )
@@ -51,23 +60,30 @@ _bug_parser.add_argument('id',
                          required=True,
                          help='Invalid data'
                          )
-_bug_parser.replace_argument('isClosed',
+_bug_parser.replace_argument('is_closed',
                             type=inputs.boolean,
                             required=True,
                             help = 'Invalid data'
                             )
+_bug_parser.replace_argument('created_at',
+                             type=inputs.date,
+                             required=True,
+                             help='Invalid data'
+                             )
 
 
 
 class Bugs(Resource):
 
     def get(self): # <- will be invoked when a GET request is made for '/bugs'
-        return BugModel.get_all()
+        return jsonify([bug.to_json() for bug in BugModel.get_all()])
 
     def post(self):
-        data = _new_bug_parser.parse_args()
-        print(data)
-        return f'bug [{data["name"]}] - is created'
+        new_bug = _new_bug_parser.parse_args()
+        new_bug_model = BugModel(**new_bug)
+        db.session.add(new_bug_model)
+        db.session.commit()
+        return jsonify(new_bug_model.to_json())
 
 class Bug(Resource):
 
@@ -88,4 +104,5 @@ api.add_resource(Bug, '/bugs/<int:id>')
 
 
 if __name__ == '__main__':
+    #db.init_app(app)
     app.run(port = 8080, debug = True)
