8d654156bb65e89c864261b1fdf7617188a4df0a
Magesh Kuppan <tkmagesh77@gmail.com>
2022-09-09 01:53:36 +0530

    sending message using SQS

--- orders-api/ordersapi/delivery.py
+++ orders-api/ordersapi/delivery.py
@@ -1,5 +1,10 @@
+import boto3
+import os
 
 
 def delivery_handler(event, context):
+    queue_client = boto3.client('sqs')
+    queue = os.environ.get('RECORD_DELIVERY_QUEUE')
+
     message = event['Records'][0]['Sns']['Message']
-    print(message)
+    queue_client.send_message(QueueUrl=queue, MessageBody="please record delievery for the order")
